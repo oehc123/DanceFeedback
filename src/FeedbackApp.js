cf5a046212a98e3b4584b0eaa71bde6cdb08e0ec
oehc123 <josesvz@gmail.com>
2020-03-10 21:29:39 -0400

    updating dependencies

--- src/FeedbackApp.js
+++ src/FeedbackApp.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import LoginPage from './screens/LoginScreen';
 
 class FeedbackApp extends Component {
     render() {
-        //this.props.navigation.push('Login') ??
         return (
             <View>
                 <Text> this is the home FeedbackApp</Text>
